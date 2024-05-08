abc258e063a134d220102792e0a54fc2b2a9687c
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-05-08 13:17:12 -0600

    Create all, any, each, each_with_index methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,51 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    my_each do |item|
+      next if yield(item)
+
+      return false
+    end
+    true
+  end
+
+  def my_any?
+    my_each do |item|
+      return true if yield(item)
+    end
+    false
+  end
+
+  def my_count
+    i = 0
+    my_each do |item|
+      if block_given?
+        i += 1 if yield(item)
+
+      else
+        i += 1
+      end
+    end
+    i
+  end
+
+  def my_each
+    for item in self
+      if block_given?
+        yield(item)
+      else
+        item
+      end
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    my_each do |item|
+      [yield(item, i)]
+      i += 1
+    end
+  end
 end
 
 # You will first have to define my_each
